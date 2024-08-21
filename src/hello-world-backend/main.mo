d7dd2b4652e5e6cade04d4eead4cd563cd750829
Anargya Widyadhana <37019778+zephyrzth@users.noreply.github.com>
2024-08-21 15:44:27 +0700

    first commit

--- src/hello-world-backend/main.mo
+++ src/hello-world-backend/main.mo
@@ -1,5 +1,11 @@
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
 actor {
   public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+    return "Hello, " # name # "! This is my first time learning ICP";
+  };
+
+  public query func add(text1 : Text) : async (Text) {
+    return "Hi" # text1 # ", nice to meet you!";
   };
 };
--- src/hello-world-frontend/src/App.jsx
+++ src/hello-world-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { hello_world_backend } from 'declarations/hello-world-backend';
 
 function App() {
   const [greeting, setGreeting] = useState('');
+  const [add, setAdd] = useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -13,9 +14,18 @@ function App() {
     return false;
   }
 
+  function handleAdd(event) {
+    event.preventDefault();
+    console.log(event.target.elements);
+    const val = event.target.elements.add.value;
+    hello_world_backend.add(val).then((resp) => {
+      setAdd(resp);
+    });
+    return false;
+  }
+
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
       <br />
       <form action="#" onSubmit={handleSubmit}>
@@ -24,6 +34,13 @@ function App() {
         <button type="submit">Click Me!</button>
       </form>
       <section id="greeting">{greeting}</section>
+      <form action="#" onSubmit={handleAdd}>
+        <label htmlFor="add">Enter some string: &nbsp;</label>
+        <input id="add" alt="Add" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="add">{add}</section>
+
     </main>
   );
 }
